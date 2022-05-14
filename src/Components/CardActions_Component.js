0f96f60c79b054393d6a77a1f369c79e3c1b976b
Mai <mainauder.new@gmail.com>
2022-05-14 06:50:09 +0200

    float

--- src/Components/CardActions_Component.js
+++ src/Components/CardActions_Component.js
@@ -3,18 +3,25 @@ import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
+import "../CSS/CardActions_CSS.css"
 
 const CardActions_Component = () => {
-    return ( <div>      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
+  return (<div className='d-flex'>
+    <CardActions disableSpacing>
+      <div className='d-flex'>
+        <div>
+        <IconButton aria-label="add to favorites" className="R" >
           <FavoriteIcon />
         </IconButton>
-        <IconButton aria-label="share">
+        </div>
+        <div>
+        <IconButton aria-label="share" className="L">
           <ShareIcon />
         </IconButton>
-
-      </CardActions>
-</div> );
+        </div>
+      </div>
+    </CardActions>
+  </div>);
 }
- 
+
 export default CardActions_Component;
